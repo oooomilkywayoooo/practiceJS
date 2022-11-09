6dc4eb63baa6b8bf9e30c781e12fc6bc080477e3
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-11-09 14:16:06 +0000

    jQuery練習

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,3 +1,10 @@
+/*global $*/
 $(function(){
-  $('.box1').slideDown();
+  $('.box1').slideDown(1000,function(){
+      $('.box1').css({
+          'background':'#0000FF',
+          'width':'200px',
+          'height':'100px'
+      }).slideUp(1000);
+  });
 });
